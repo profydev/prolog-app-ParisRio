d16ddb517301496202265861238416fceed3ca0c
Clément <69433605+ParisRio@users.noreply.github.com>
2022-11-28 21:30:28 +0100

    Refactor code - unused function and index.ts for hook folder (#9)
    * remove unused Capitalize import
    
    * add index.ts file for export to hook folder

--- features/projects/components/project-card/project-card.tsx
+++ features/projects/components/project-card/project-card.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import styled from "styled-components";
-import capitalize from "lodash/capitalize";
 import { Badge, BadgeColor } from "@features/ui";
 import {
   Project,
