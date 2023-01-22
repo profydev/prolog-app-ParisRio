bce242b04ce29adc40ebb1af17623656d8dfef43
Clément Bréan <Clem@clement.home>
2023-01-22 17:35:29 +0100

    first version of the alert message in the project list

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ProjectCard } from "../project-card";
 import { useProjects } from "../../api/use-projects";
-import { breakpoint, space } from "@styles/theme";
+import { breakpoint, color, space, textFont } from "@styles/theme";
 import { LoadingIndicator } from "@features/ui";
 
 const List = styled.ul`
@@ -27,8 +27,48 @@ const LoadingIndicatorContainer = styled.div`
   }
 `;
 
+const AlertContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${space(4)};
+  border-radius: ${space(2)};
+  border: 1px solid ${color("error", 300)};
+  background-color: ${color("error", 25)};
+  color: ${color("error", 700)};
+  ${textFont("sm", "medium")};
+`;
+
+const AlertIcon = styled.img`
+  padding-right: ${space(4)};
+`;
+const AlertMessage = styled.div`
+  color: ${color("error", 700)};
+`;
+const AlertButton = styled.div`
+  display: flex;
+  margin-left: auto;
+`;
+const AlertButtonText = styled.div`
+  color: ${color("error", 700)};
+  padding-right: ${space(3)};
+`;
+const AlertButtonArrow = styled.img``;
+
 export function ProjectList() {
   const { data, isLoading, isError, error } = useProjects();
+
+  return (
+    <AlertContainer>
+      <AlertIcon src="/icons/alert-circle.svg" alt="logo" />
+      <AlertMessage>
+        There was a problem while loading the project data
+      </AlertMessage>
+      <AlertButton>
+        <AlertButtonText>Try again</AlertButtonText>
+        <AlertButtonArrow src="/icons/alert-arrow.svg" alt="logo" />
+      </AlertButton>
+    </AlertContainer>
+  );
 
   if (isLoading) {
     return (
@@ -40,7 +80,19 @@ export function ProjectList() {
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    //return <div>Error: {error.message}</div>;
+    return (
+      <AlertContainer>
+        <AlertIcon src="/icons/alert-circle.svg" alt="logo" />
+        <AlertMessage>
+          There was a problem while loading the project data
+        </AlertMessage>
+        <AlertButton>
+          <AlertButtonText>Try again</AlertButtonText>
+          <AlertButtonArrow src="/icons/alert-arrow.svg" alt="logo" />
+        </AlertButton>
+      </AlertContainer>
+    );
   }
 
   return (
