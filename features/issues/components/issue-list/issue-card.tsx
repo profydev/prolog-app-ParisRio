447936c839d3a99e32a0fb66859c8c10313529a5
Clément Bréan <Clem@clement.home>
2023-07-10 22:54:19 +0200

    styling and text overflow hidden for error messages

--- features/issues/components/issue-list/issue-card.tsx
+++ features/issues/components/issue-list/issue-card.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import styled from "styled-components";
-import { color, space, textFont, displayFont } from "@styles/theme";
+import { color, space, textFont } from "@styles/theme";
 import { Badge, BadgeColor, BadgeSize } from "@features/ui";
 import { IssueLevel } from "../../types/issue.types";
 import { ProjectLanguage } from "@features/projects";
@@ -22,44 +21,56 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  margin-bottom: ${space(4)};
+  padding: ${space(6)};
   background: white;
   box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
     0px 1px 2px rgba(16, 24, 40, 0.06);
   border-radius: 8px;
 `;
 
-const TopContainer = styled.div`
-  padding: ${space(6)};
+//Parent container for the callstack message that need the following property for text overflow to work
+//https://stackoverflow.com/questions/17779293/css-text-overflow-ellipsis-not-working
+//So if you reach this question because you're having trouble trying to get the ellipsis working
+// inside a display: flex container, try adding min-width: 0 to the outmost container that's overflowing
+//its parent even though you already set a overflow: hidden to it and see how that works for you
+const ErrorContainer = styled.div`
+  min-width: 0;
 `;
 
-const BottomContainer = styled.div`
-  padding: ${space(4, 6)};
-  border-top: 1px solid ${color("gray", 200)};
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const NameAndIconContainer = styled.div`
+const ErrorAndIconContainer = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: ${space(6)};
+  margin-bottom: ${space(10)};
 `;
 
 const LanguageIcon = styled.img`
   margin-right: ${space(3)};
 `;
 
-const Name = styled.div`
-  ${textFont("md", "medium")}
+const ErrorTypeAndMessage = styled.div`
+  color: ${color("gray", 900)};
+  white-space: nowrap; /* keep text on a single line */
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
-const Language = styled.div`
+const ErrorType = styled.span`
+  ${textFont("sm", "medium")}
+`;
+
+const StackMessage = styled.div`
   color: ${color("gray", 500)};
   ${textFont("sm", "regular")}
+  min-width: 0;
+  white-space: nowrap; /* keep text on a single line */
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const InfoContainer = styled.div`
   display: flex;
+  justify-content: center;
 `;
 
 const Issues = styled.div`
@@ -74,26 +85,13 @@ const IssuesTitle = styled.div`
   margin-bottom: ${space(2)};
   color: ${color("gray", 500)};
   ${textFont("sm", "medium")}
+  text-align: center;
 `;
 
 const IssuesNumber = styled.div`
-  ${displayFont("md", "semibold")}
-`;
-
-const Status = styled.div`
-  // line-height of issue number element
-  height: 2.75rem;
-  margin-top: auto;
-  margin-left: ${space(6)};
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  flex: 1;
-`;
-
-const ViewIssuesAnchor = styled.a`
-  text-decoration: none;
-  ${textFont("sm", "medium")}
+  color: ${color("gray", 500)};
+  ${textFont("sm", "regular")}
+  text-align: center;
 `;
 
 export function IssueCard({ projectLanguage, issue }: IssueRowProps) {
@@ -101,35 +99,35 @@ export function IssueCard({ projectLanguage, issue }: IssueRowProps) {
   const firstLineOfStackTrace = stack.split("\n")[1];
   return (
     <Container>
-      <TopContainer>
-        <NameAndIconContainer>
-          <LanguageIcon
-            src={`/icons/${projectLanguage}.svg`}
-            alt={projectLanguage}
-          />
-          <div>
-            <Name>{name}:&nbsp;</Name>
-            <div>{message}</div>
-            <Language>{firstLineOfStackTrace}</Language>
-          </div>
-        </NameAndIconContainer>
-        <InfoContainer>
-          <Issues>
-            <IssuesTitle>Status</IssuesTitle>
-            <Badge color={levelColors[level]} size={BadgeSize.sm}>
-              {capitalize(level)}
-            </Badge>
-          </Issues>
-          <Issues>
-            <IssuesTitle>Event</IssuesTitle>
-            <IssuesNumber>{numEvents}</IssuesNumber>
-          </Issues>
-          <Issues>
-            <IssuesTitle>Users</IssuesTitle>
-            <IssuesNumber>{numUsers}</IssuesNumber>
-          </Issues>
-        </InfoContainer>
-      </TopContainer>
+      <ErrorAndIconContainer>
+        <LanguageIcon
+          src={`/icons/${projectLanguage}.svg`}
+          alt={projectLanguage}
+        />
+        <ErrorContainer>
+          <ErrorTypeAndMessage>
+            <ErrorType>{name}:&nbsp;</ErrorType>
+            {message}
+          </ErrorTypeAndMessage>
+          <StackMessage>{firstLineOfStackTrace}</StackMessage>
+        </ErrorContainer>
+      </ErrorAndIconContainer>
+      <InfoContainer>
+        <Issues>
+          <IssuesTitle>Status</IssuesTitle>
+          <Badge color={levelColors[level]} size={BadgeSize.sm}>
+            {capitalize(level)}
+          </Badge>
+        </Issues>
+        <Issues>
+          <IssuesTitle>Event</IssuesTitle>
+          <IssuesNumber>{numEvents}</IssuesNumber>
+        </Issues>
+        <Issues>
+          <IssuesTitle>Users</IssuesTitle>
+          <IssuesNumber>{numUsers}</IssuesNumber>
+        </Issues>
+      </InfoContainer>
     </Container>
   );
 }
